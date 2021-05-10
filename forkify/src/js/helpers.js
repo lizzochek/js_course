18182254db374cc37e842bf7eb45baf56a540b84
lizzochek <littled685@gmail.com>
2021-05-10 20:33:12 +0300

    Sending data to the API
    To create a new recipe we send the data to the API on upload

--- forkify/src/js/helpers.js
+++ forkify/src/js/helpers.js
@@ -19,3 +19,23 @@ export const getJSON = async function (url) {
     throw err;
   }
 };
+
+export const sendJSON = async function (url, dataToSend) {
+  try {
+    const fetchUrl = fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+    });
+
+    const res = await Promise.race([fetchUrl, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
--- forkify/src/js/model.js
+++ forkify/src/js/model.js
@@ -1,6 +1,6 @@
 import { async } from "regenerator-runtime";
-import { API_URL, RES_PER_PAGE } from "./config.js";
-import { getJSON } from "./helpers.js";
+import { API_URL, RES_PER_PAGE, KEY } from "./config.js";
+import { getJSON, sendJSON } from "./helpers.js";
 
 export const state = {
   recipe: {},
@@ -13,21 +13,25 @@ export const state = {
   bookmarks: [],
 };
 
+const createRecipe = (data) => {
+  let { recipe } = data.data;
+  return {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+};
+
 export const loadRecipe = async function (id) {
   try {
     const data = await getJSON(`${API_URL}${id}`);
+    state.recipe = createRecipe(data);
 
-    let { recipe } = data.data;
-    state.recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
     state.recipe.bookmarked = state.bookmarks.some(
       (bookmark) => bookmark.id === id
     )
@@ -109,8 +113,21 @@ export const uploadRecipe = async (newRecipe) => {
           );
 
         const [quantity, unit, description] = ings;
+
         return { quantity: quantity ? +quantity : null, unit, description };
       });
+    const recipe = {
+      title: newRecipe.title,
+      source_url: newRecipe.sourceUrl,
+      image_url: newRecipe.image,
+      publisher: newRecipe.publisher,
+      cooking_time: +newRecipe.cookingTime,
+      servings: +newRecipe.servings,
+      ingredients,
+    };
+
+    sendJSON(`${API_URL}?key=${KEY}`, recipe);
+    state.recipe = createRecipe(data);
   } catch (err) {
     throw err;
   }
